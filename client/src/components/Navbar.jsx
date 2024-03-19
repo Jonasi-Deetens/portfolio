08645a8380a3c6c746074b46ab6c0e14e95593ea
TheNyxs <jonasideetens@hotmail.com>
2024-03-20 00:12:09 +0100

    Add routes for home page and courses

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,9 +4,10 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='flex justify-between h-20 px-10 py-5 bg-gray-50'>
-        <h1>Portfolio</h1>
+        <h1>Jonasi Deetens</h1>
         <ul className='flex items-center'>
-            <NavLink exact to="/courses" className="link" activeClassName=''>Courses</NavLink>
+            <NavLink to="/" className="link" activeClassName='active'>Home</NavLink>
+            <NavLink to="/courses" className="link" activeClassName='active'>Courses</NavLink>
         </ul>
         <button className='px-4 flex items-center'><NavLink exact to="/login" className="text-neutral-50 hover:text-neutral-50" activeClassName=''>Login</NavLink></button>
     </nav>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+        <h2>HOME</h2>
+        <section className='flex items-center justify-around'>
+
+        </section>
+    </main>
+  )
+}
+
+export default Home
